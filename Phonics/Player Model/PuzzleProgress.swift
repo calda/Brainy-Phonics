9d677c70d060b8c6f781e99cc604386f1ab75b15
Cal Stephens <cal@calstephens.tech>
2016-12-25 23:23:34 -0500

    Clean up Words folder and relink as necessary

--- Phonics/Player Model/PuzzleProgress.swift
+++ Phonics/Player Model/PuzzleProgress.swift
@@ -14,6 +14,7 @@ class PuzzleProgress: NSObject, NSCoding {
     var ownedPieces: [[Bool]]
     
     var isComplete: Bool {
+        return true
         var isComplete = true
         
         for column in ownedPieces {
