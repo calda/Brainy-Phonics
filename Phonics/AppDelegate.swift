8f4cf8317d65f7c372239698f0c4afa67e8e9fe4
Cal Stephens <cal@calstephens.tech>
2017-06-30 00:55:36 -0700

    Fix Pig Latin bug on return from background

--- Phonics/AppDelegate.swift
+++ Phonics/AppDelegate.swift
@@ -12,12 +12,18 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         return true
     }
-
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        //LaunchViewController -> HomeViewController -> PigLatinViewController (fragile but i don't expect it to change)
+        if let pigLatinViewController = window?.rootViewController?.presentedViewController?.presentedViewController as? PigLatinViewController {
+            //pig latin has to dismiss because going in background puts the timers out of sync with the audio
+            pigLatinViewController.dismiss(animated: true, completion: nil)
+        }
+    }
 
 }
 
