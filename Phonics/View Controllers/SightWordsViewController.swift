008732e55ac7e61e3f6ac8705d1304ce4250ece9
Will Murat Said <will.said7@gmail.com>
2018-06-17 20:45:13 -0400

    changed 20 rhymes, and made minor improvments/bug fixes (like autolayout)

--- Phonics/View Controllers/SightWordsViewController.swift
+++ Phonics/View Controllers/SightWordsViewController.swift
@@ -62,7 +62,7 @@ class SightWordsViewController : UIViewController, UICollectionViewDataSource, U
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = (self.view.frameInsetByMargins.width - 50) / 3
+        let width = (self.view.frameInsetByMargins.width - 100) / 3
         return CGSize(width: width, height: width * 0.85)
     }
     
